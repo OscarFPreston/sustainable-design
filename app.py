538735eb7d8087a0dce6e46f2c0b342786972a58
ellyrose <elly_turnbull@hotmail.co.uk>
2022-06-19 15:49:57 +0100

    create about route

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
-
+from flask import Flask,  render_template
 
 app = Flask(__name__)
-
-
 
 @app.errorhandler(404)
 def page_not_found(e):
    return render_template('404.html'), 404
    
 @app.route('/')
-def app():
+def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
 
 
 
